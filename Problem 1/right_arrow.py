e568007183dd3bb52ca17af52f44309ebd0277ed
madpoetwithapen <christinekparrish2023@gmail.com>
2024-02-02 15:11:49 -0600

    Arrow Check

--- Problem 1/right_arrow.py
+++ Problem 1/right_arrow.py
@@ -3,13 +3,17 @@ def right_arrow():
     head_char = input()
 
     row1 = '      ' + head_char
+    row2 = base_char+base_char+base_char+base_char+base_char+base_char+head_char+head_char 
+    row3 = base_char+base_char+base_char+base_char+base_char+base_char+head_char+head_char+head_char
+    row4 = base_char+base_char+base_char+base_char+base_char+base_char+head_char
+    
     ''' Type your code here. '''
 
     print(row1)
     print(row2)
     print(row3)
     print(row2)
-    print(row1)
+    print(row4)
 
 if __name__ == "__main__":
     right_arrow()
