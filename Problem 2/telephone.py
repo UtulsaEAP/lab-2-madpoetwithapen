35b8e8741cd3a4b26c3c6e33f611bd7dfad4ee14
madpoetwithapen <christinekparrish2023@gmail.com>
2024-02-09 15:43:29 -0600

    I'm so tired

--- Problem 2/telephone.py
+++ Problem 2/telephone.py
@@ -1,16 +1,19 @@
 def telephone():
     
     ''' Type your code here. '''
-    
-    firstDig = str(int(phone_number / 10000000))
+
+    #input(int(phone_number))
+    phone_number=int(input())
+
+    firstDig = int(phone_number / 10000000)
     print(firstDig)
     
     phone_number = phone_number % 10000000
     print(phone_number)
     
-    middleDig = str(int(phone_number / 10000))
+    middleDig = int(phone_number / 10000)
     
-    lastDig = str(int(phone_number %10000))
+    lastDig = int(phone_number %10000)
     
     print("(" + firstDig + ") "+ middleDig + "-" + lastDig)
     
