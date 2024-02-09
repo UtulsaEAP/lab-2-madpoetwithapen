924171c4151405bd3d9f33327a4d90578b0d4570
madpoetwithapen <christinekparrish2023@gmail.com>
2024-02-09 15:34:13 -0600

    Again

--- Problem 4/real_estate.py
+++ Problem 4/real_estate.py
@@ -9,7 +9,7 @@ def real_estate():
     price_change=((current_price) - (last_month_price))
     mortage=((current_price * 0.051) / 12)
 
-    print (f"This house is ${current_price} The change is ${price_change} since last month.")
+    print (f"This house is ${current_price}. The change is ${price_change} since last month.")
     #print ("The estimated monthly mortage is"+f'{mortage:.2f}'+".")
     print(f"The estimated monthly mortgage is ${mortage:.2f}.")
     # print(f"{current_price} is an int {price_change} is also an int")
