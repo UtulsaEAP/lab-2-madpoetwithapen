7e433f50a3ff8af456fd130eacaf5404cc498b78
madpoetwithapen <christinekparrish2023@gmail.com>
2024-02-09 15:46:42 -0600

    how many times can I redo this

--- Problem 2/telephone.py
+++ Problem 2/telephone.py
@@ -6,16 +6,14 @@ def telephone():
     phone_number=int(input())
 
     firstDig = int(phone_number / 10000000)
-    print(firstDig)
     
     phone_number = phone_number % 10000000
-    print(phone_number)
     
     middleDig = int(phone_number / 10000)
     
     lastDig = int(phone_number %10000)
     
-    print("(" + firstDig + ") "+ middleDig + "-" + lastDig)
+    print(f'({firstDig}) {middleDig}-{lastDig}')
     
 
 if __name__ == "__main__":
