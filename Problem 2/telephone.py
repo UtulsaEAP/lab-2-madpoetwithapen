1dc47e26ddd375800ca6f1ea0e19bfeb91aabf79
madpoetwithapen <christinekparrish2023@gmail.com>
2024-02-09 15:37:08 -0600

    final telephone sync

--- Problem 2/telephone.py
+++ Problem 2/telephone.py
@@ -1,7 +1,18 @@
 def telephone():
     
     ''' Type your code here. '''
-
+    
+    firstDig = str(int(phone_number / 10000000))
+    print(firstDig)
+    
+    phone_number = phone_number % 10000000
+    print(phone_number)
+    
+    middleDig = str(int(phone_number / 10000))
+    
+    lastDig = str(int(phone_number %10000))
+    
+    print("(" + firstDig + ") "+ middleDig + "-" + lastDig)
     
 
 if __name__ == "__main__":
