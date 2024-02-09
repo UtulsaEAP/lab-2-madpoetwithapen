21c6a3bbc83ffdc650f672192e4f1d8cb44df201
madpoetwithapen <christinekparrish2023@gmail.com>
2024-02-09 15:58:03 -0600

    Final sync

--- Problem 5/simple_stats.py
+++ Problem 5/simple_stats.py
@@ -5,5 +5,16 @@ def simple_stats():
     num3 = float(input())
     num4 = float(input())
     ''' Type your code here. '''
+
+    import math
+
+    product = (num1 * num2 *num3 * num4)
+    sum = (num1 + num2 + num3 + num4)
+    average = (sum/4)
+
+    print(f'{product:.0f} {average:.0f}') 
+    print(f'{product:.3f} {average:.3f}')
+
+
 if __name__ == "__main__":
     simple_stats()
